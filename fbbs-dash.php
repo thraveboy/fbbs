ebd9f5db7b71db166b65f2eea9aeefdbd5fcae2c
Fury <reid.gershbein@gmail.com>
2016-12-18 17:52:43 +0000

    Initial fdb setup files.

---
+++ fbbs-dash.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    background-color: blue;
+    color: cyan;
+}
+</style>
+</head>
+
+<body>
+<?php
+  $_LOCAL_API_CALLS = 1;
+  require 'api.php';
+
+  $previous_command = $_GET['command'];
+  echo '<p>';
+  echo '<div id="previous_command">';
+  print($previous_command);
+  echo '</div>';
+  echo '</p>';
+?>
+
+<FORM NAME="form1" METHOD="GET" ACTION="dash.php">
+    <INPUT TYPE="Text" VALUE="" id="command" NAME="command" SIZE="80" autofocus
+           onkeyup="showDash(this.value)">
+</FORM>
+
+<p>Dash
+<br>
+<span id="dash"></span>
+</p>
+
+<script>
+function showDash(str) {
+  var xhttp;
+  if (str.length == 0) {
+    document.getElementById("dash").innerHTML = "=-=";
+    return;
+  }
+  xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("dash").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "fbbs-index.php?command="+str, true);
+  xhttp.send();
+}
+
+var prev_cmd_val = document.getElementById("previous_command").innerText;
+
+if (prev_cmd_val) {
+ showDash(prev_cmd_val);
+ document.getElementById("command").value = prev_cmd_val;
+}
+
+function updateDash() {
+  var dashName = document.getElementById("command").value;
+  if (dashName) {
+    showDash(dashName);
+  }
+}
+
+var dashUpdater = setInterval(updateDash, 5000);
+
+</script>
+
+</body>
+</html>
---
+++ fbbs-index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    background-color: blue;
+    color: cyan;
+}
+</style>
+</head>
+
+<body>
+<?php
+  $_LOCAL_API_CALLS = 1;
+  require 'api.php';
+
+  $previous_command = $_GET['command'];
+  echo '<p>';
+  print($previous_command);
+  echo '</p>';
+?>
+
+<?php
+   $name = '(anonymous - type "name '. '(yourname)' . '")';
+   if ($ip_name) {
+     $name = $ip_name;
+   }
+   echo '<p>';
+   echo '[' . $_SERVER['REMOTE_ADDR']  . ' - ' . $name . '] ';
+   echo '</p>';
+?>
+
+<FORM NAME="form1" METHOD="get" ACTION="">
+    <INPUT TYPE="Text" VALUE="" NAME="command" SIZE="80" autofocus>
+</FORM>
+
+<?php
+  $exploded_previous_command = explode(" ", $previous_command, 2);
+  $arg_count = count($exploded_previous_command);
+  if ($arg_count == 1) {
+    $json_str = $outputObject->retrieve();
+    $json_obj = json_decode($json_str);
+    $json_value_array = $json_obj->value;
+    foreach ($json_value_array as $value_obj) {
+      echo '<p>';
+      foreach ($value_obj as $property) {
+        echo '<p>';
+        foreach ($property as $row) {
+          foreach (get_object_vars($row) as $key => $value) {
+            echo $key . ' <b>' . $value . '</b><br>';
+          }
+        }
+        echo '</p>';
+      }
+      echo '</p>';
+    }
+  }
+  if ($arg_count == 2) {
+    $json_str = $outputObject->retrieve();
+    $json_obj = json_decode($json_str);
+    $json_value_array = $json_obj->value;
+    echo '<p>';
+    foreach ($json_value_array as $value_obj) {
+      foreach ($value_obj as $key => $value) {
+        echo '<b>';
+        echo $key . '</b> ' . $value . '<br>';
+      }
+    }
+    echo '</p>';
+  }
+?>
+
+</body>
+</html>
