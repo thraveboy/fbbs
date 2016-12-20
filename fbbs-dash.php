ae6839ab939be33b081406fe63c76c69f3fe6358
Fury <reid.gershbein@gmail.com>
2016-12-20 01:54:11 +0000

    Dashboards now working, yay!

--- fbbs-dash.php
+++ fbbs-dash.php
@@ -32,7 +32,11 @@ body {
 <span id="dash"></span>
 </p>
 
+
 <script>
+
+var prev_cmd_val = document.getElementById("previous_command").innerText;
+
 function showDash(str) {
   var xhttp;
   if (str.length == 0) {
@@ -42,14 +46,38 @@ function showDash(str) {
   xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("dash").innerHTML = this.responseText;
+      document.getElementById("dash").innerHTML = "<p><b>" + prev_cmd_val +
+                                                  "</b></p>";
+      var current_time = (new Date()).getTime();
+      var jsonresponseobj = JSON.parse(this.responseText).value[0];
+      Object.keys(jsonresponseobj).forEach(function(key,index) {
+        var dash_entries = jsonresponseobj[key].length;
+        for (var entry_index=0; entry_index < dash_entries; entry_index++) {
+          var dash_entry = jsonresponseobj[key][entry_index];
+          Object.keys(dash_entry).forEach(function(key2,index2) {
+             var dash_val = dash_entry[key2];
+             var output_val = '';
+             switch(key2) {
+               case 'value':
+                 output_val = '<b>' + dash_val + '</b> ';
+                 break;
+               case 'timestamp':
+                 var dashtime = ((current_time/1000) - dash_val) / 3600;
+                 output_val = (Math.trunc((dashtime*1000)))/1000 + ' hours ago';
+                 break;
+               default:
+                 break;
+             }
+             document.getElementById("dash").innerHTML += output_val;
+            });
+        };
+        document.getElementById("dash").innerHTML += '<br>';
+      });
     }
-  };
-  xhttp.open("GET", "fbbs-index.php?command="+str, true);
+  }
+  xhttp.open("GET", "fbbs-api.php?command="+str, true);
   xhttp.send();
 }
-
-var prev_cmd_val = document.getElementById("previous_command").innerText;
 
 if (prev_cmd_val) {
  showDash(prev_cmd_val);
